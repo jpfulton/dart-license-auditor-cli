cc2102d474d1200c967ba000178724c17edd982b
J. Patrick Fulton <patrick@lockboxai.com>
2025-01-23 13:40:31 -0600

    Refactor DangerJS plugin to transition from Java to Dart license auditing
    - Replaced the Java license auditor with the Dart license auditor in the DangerJS plugin.
    - Updated exports to reflect the new Dart functionality.
    - This change aligns the plugin with Dart project requirements and enhances maintainability.

--- src/danger/index.ts
+++ src/danger/index.ts
@@ -1,5 +1,5 @@
 import type { IPluginConfig } from "./danger-plugin.js";
-import { javaLicenseAuditor } from "./danger-plugin.js";
+import { dartLicenseAuditor } from "./danger-plugin.js";
 
-export { javaLicenseAuditor };
+export { dartLicenseAuditor };
 export type { IPluginConfig };
